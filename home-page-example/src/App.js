e2e268008860855028c6600bd4c2baa1eec19cb7
maxrosario99 <maxumum1000@gmail.com>
2024-02-19 13:51:09 -0500

    some message

---
+++ home-page-example/src/App.js
@@ -0,0 +1,28 @@
+
+import "./App.css";
+import Footer from "./components/Footer";
+import MyNavBar from "./components/MyNavBar";
+import Welcome from "./components/Welcome";
+//import Contact from ".fff/pages/Contact";
+
+function App() {
+  return (
+    <div>
+      
+      
+      
+     
+      
+      
+      
+      
+      
+      
+        <MyNavBar />
+       <Welcome />
+       <Footer /> 
+     </div>
+  );
+}
+
+export default App;
---
+++ home-page-example/src/components/MyNavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const MyNavBar = () => {
+  return (
+    <ul id="nav-container">
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+      <li>
+        <a href="#">Info</a>
+      </li>
+    </ul>
+  );
+};
+
+export default MyNavBar;
